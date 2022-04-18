e777518fd3b6d5b049ac5df506ea7939a22ae5d6
Jia Jia <jia.jia@zte.com.cn>
2022-04-18 22:22:51 +0800

    Fix lint error

--- gerritaction/logger/logger.py
+++ gerritaction/logger/logger.py
@@ -12,7 +12,7 @@ class Logger(object):
     @staticmethod
     def debug(msg):
         sys.stderr.write(
-            u"{debug}{time} DEBUG:{reset} {msg}\n".format(
+            "{debug}{time} DEBUG:{reset} {msg}\n".format(
                 debug=colorama.Fore.GREEN + colorama.Style.BRIGHT,
                 time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
                 reset=colorama.Style.RESET_ALL,
@@ -23,7 +23,7 @@ class Logger(object):
     @staticmethod
     def error(msg):
         sys.stderr.write(
-            u"{error}{time} ERROR:{reset} {msg}\n".format(
+            "{error}{time} ERROR:{reset} {msg}\n".format(
                 error=colorama.Fore.RED + colorama.Style.BRIGHT,
                 time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
                 reset=colorama.Style.RESET_ALL,
@@ -34,7 +34,7 @@ class Logger(object):
     @staticmethod
     def info(msg):
         sys.stderr.write(
-            u"{info}{time} INFO:{reset} {msg}\n".format(
+            "{info}{time} INFO:{reset} {msg}\n".format(
                 info=colorama.Fore.WHITE + colorama.Style.BRIGHT,
                 time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
                 reset=colorama.Style.RESET_ALL,
@@ -45,7 +45,7 @@ class Logger(object):
     @staticmethod
     def warn(msg):
         sys.stderr.write(
-            u"{warn}{time} WARN:{reset} {msg}\n".format(
+            "{warn}{time} WARN:{reset} {msg}\n".format(
                 warn=colorama.Fore.YELLOW + colorama.Style.BRIGHT,
                 time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),
                 reset=colorama.Style.RESET_ALL,
